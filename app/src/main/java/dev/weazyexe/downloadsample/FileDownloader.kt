dd12180ed9295e1ca6ef5e095dd33e43eae475f9
Timur Zadvornov <weazy.exe@gmail.com>
2022-01-14 12:20:13 +0300

    added support of multiple files

---
+++ app/src/main/java/dev/weazyexe/downloadsample/FileDownloader.kt
@@ -0,0 +1,63 @@
+package dev.weazyexe.downloadsample
+
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
+import android.os.Environment
+import android.util.Log
+
+private typealias FileId = Long
+
+/**
+ * File downloader via [DownloadManager]
+ *
+ * @param context application context
+ */
+class FileDownloader(private val context: Context) {
+
+    private val downloadingFiles = HashMap<FileId, Boolean>()
+
+    /**
+     * Download a file
+     *
+     * @param url file URL
+     * @param fileName file name in device's file system
+     */
+    fun download(url: String, fileName: String) {
+        val receiver = prepareReceiver()
+        val request = prepareRequest(Uri.parse(url), fileName)
+        val manager = prepareDownloadManager()
+
+        val fileId = manager.enqueue(request)
+        downloadingFiles[fileId] = true
+        context.registerReceiver(
+            receiver,
+            IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
+        )
+    }
+
+    private fun prepareReceiver(): BroadcastReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if (id in downloadingFiles) {
+                Log.d("DOWNLOADER", "Downloading file with ID=$id has finished!")
+                downloadingFiles.remove(id)
+            }
+            context?.unregisterReceiver(this)
+        }
+    }
+
+    private fun prepareRequest(uri: Uri, fileName: String): DownloadManager.Request =
+        DownloadManager.Request(uri)
+            .setTitle(fileName)
+            .setDescription(fileName)
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+
+    private fun prepareDownloadManager(): DownloadManager =
+        context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+}
